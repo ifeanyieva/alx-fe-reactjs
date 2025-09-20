a5bef9f94b5b49453df83d5fa07f4f28dca5e285
ifeanyieva <evailomuanya95@gmail.com>
2025-09-20 16:55:01 +0100

    new modifications

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import WelcomeMessage from './components/WelcomeMessage'
+
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +12,7 @@ function App() {
   return (
     <>
       <div>
+        <WelcomeMessage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
