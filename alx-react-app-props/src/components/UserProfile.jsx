766c7eb1ba74742404cf5a3963f7e4e5eaae7d65
ifeanyieva <evailomuanya95@gmail.com>
2025-09-24 06:01:05 +0100

    modified user profile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,17 @@
-const UserProfile = (props) => {
-   
-    return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
- export default UserProfile;
+const UserProfile = () => {
+  // Get user data from context
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {userData.age}</p>
+      <p>Bio: {userData.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
