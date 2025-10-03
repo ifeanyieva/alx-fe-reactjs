ca170955e830ebde073b8f9097f7ee6b880cb5f9
ifeanyieva <evailomuanya95@gmail.com>
2025-10-03 18:49:48 +0100

    new api call url

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const GITHUB_BASE_URL = "https://api.github.com";
+const GITHUB_BASE_URL = "https://api.github.com/search/users?q";
 
 export const fetchUserData = async (
   username,
